7685fb704001e61846bf6342171aa549820b492e
Eveieve <jhkdev1@gmail.com>
2025-04-05 15:45:41 +0900

    회원 도메인 설계의 문제점 메모

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 실제 할당하는 부분 (new MemooryMemberRepository()) --> 구현체에 의존(구현체를 알고있음)
+    // 추상화에도 의존(MemberRepository) 하고 구체화(MemoryMemberRepository)에도 의존하고 있음! ==> DIP 위반!!
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
