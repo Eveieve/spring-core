cf62bea88241f4834bccfc278a9c31fd065542f5
Eveieve <jhkdev1@gmail.com>
2025-04-06 20:19:21 +0900

    test: 특정 타입을 모두 조회하기

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -3,6 +3,7 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -30,6 +33,16 @@ public class ApplicationContextSameBeanFindTest {
         // 1번 꺼내고 싶으면 memberRepository1 명시하기
         MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType =  ac.getBeansOfType(MemberRepository.class);
+        for(String key: beansOfType.keySet()) {
+            System.out.println("key = " + key +  " value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
     }
 
     @Configuration
